bf600ac32bd60ddee61f6780067deb937a9b369d
youlei1 <2730027357@qq.com>
2024-06-26 16:48:34 +0800

    修改密码加背景

--- src/lib/changePassword.py
+++ src/lib/changePassword.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from src.classes.User import User
 def update():
     return True
+def back():
+    return True
 def changePassword(user:User):
     root = tk.Tk()
     root.wm_attributes("-topmost", True)
@@ -17,13 +19,20 @@ def changePassword(user:User):
     lambdaUsername = tk.Label(root,text='新用户名:',font=('宋体',16))
     lambdaUsername.place(x=10,y=20)
     lambdapassword = tk.Label(root,text='新密码：',font=('宋体',16))
-    lambdapassword.place(x=15,y=80)
+    lambdapassword.place(x=15,y=60)
+    repassword = tk.Label(root,text='再次输入新密码：',font=('宋体',16))
+    repassword.place(x=0,y=100)
     username=tk.StringVar(root,value='')
     password=tk.StringVar(root,value='')
+    repassword=tk.StringVar(root,value='')
     e_name=tk.Entry(root,width=25,textvariable=username)
     e_name.place(x=120,y=25)
     e_pwd=tk.Entry(root,width=25,show='*',textvariable=password)
-    e_pwd.place(x=120,y=85)
+    e_pwd.place(x=120,y=65)
+    repwd=tk.Entry(root,width=25,show='*',textvariable=password)
+    repwd.place(x=160,y=105)
+    b2=tk.Button(root,text='返回',command=back)
+    b2.place(x=120,y=145)
     b1=tk.Button(root,text='提交',command=update)
-    b1.place(x=200,y=140)
+    b1.place(x=220,y=145)
     root.mainloop()
